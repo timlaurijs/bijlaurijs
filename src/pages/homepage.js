ae0256f1433505578bcb8e260380e8795d548b3a
timlaurijs <timlaurijs@gmail.com>
2020-07-25 03:50:16 +0200

    succesfully fetched data from cms

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,22 +1,91 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Switch, Route } from "react-router-dom"
 import { fetchMenuItems } from "../store/menubar/actions"
 import { selectMenuItems } from "../store/menubar/selectors"
 import "./homepage.scss"
+import sanity from "@sanity/client"
 import Menubar from "../components/menubar"
 import Images from "../components/images"
+const { NODE_ENV, REACT_APP_SANITY_PROJECT_ID: PROJECT_ID } = process.env
 
 export default function Homepage() {
-  const dispatch = useDispatch()
-  const menuItems = useSelector(selectMenuItems)
-  console.log("what is menuItems?", menuItems)
+  const [posts, setPosts] = useState([])
+  const [images, setImages] = useState([])
 
+  const sanityClient = require("@sanity/client")
+  const client = sanityClient({
+    projectId: PROJECT_ID,
+    dataset: "production",
+    useCdn: NODE_ENV === "production",
+  })
+
+  const getPostQuery = `
+  *[_type=="post"] | order(order asc){
+    title,
+    body,
+  }
+  `
+  const getImagesQuery = `
+  * [_type=="afbeeldingen"]{
+    _id,
+    "image": mainImage.asset->,
+    post->,
+    seasons
+  }
+  `
   useEffect(() => {
-    dispatch(fetchMenuItems())
-  }, [dispatch])
+    client.fetch(getImagesQuery).then((images) => {
+      // console.log("images 1", images)
+      setImages(images)
+    })
+    client.fetch(getPostQuery).then((posts) => {
+      // console.log("Posts 1", posts)
+      setPosts(posts)
+    })
+  }, [])
+
+  const pictures = images.map((image) => {
+    return image.image.path
+  })
+
+  console.log(pictures)
+  console.log(images)
+
+  //image.path
+
+  const postBody = posts.map((post) => {
+    return `${post.title}
+            ${post.body[0].children[0].text}`
+  })
+  const postTitle = posts.map((post) => {
+    return post.title
+  })
 
   return (
+    <div className="container">
+      <div className="grid">
+        <div className="item">
+          {posts.map((post) => {
+            return (
+              <div>
+                <h2>{post.title}</h2>
+                <p>{post.body[0].children[0].text}</p>
+              </div>
+            )
+          })}
+        </div>
+        <div className="images">
+          {images.map((image) => {
+            return <img className="image" src={image.image.url} />
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+/*
     <div className="grid">
       <div className="menubar">
         <div className="menubar-inner">
@@ -34,5 +103,4 @@ export default function Homepage() {
         })}
       </div>
     </div>
-  )
-}
+*/
