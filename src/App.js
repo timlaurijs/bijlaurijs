91585d7baa61b71ba618b4abf7a11f3a8a365950
timlaurijs <timlaurijs@gmail.com>
2020-07-28 14:54:43 +0200

    Cleaning up unused code & renaming images to timeline

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect } from "react"
 import "./App.scss"
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
 import { fetchTimeline, fetchChapters } from "./store/homepage/actions"
 import { Switch, Route } from "react-router-dom"
-import { fetchMenuItems } from "./store/menubar/actions"
 import Homepage from "./pages/homepage"
-import Admin from "./pages/admin"
-import Login from "./pages/login"
 
 export default function App() {
   const dispatch = useDispatch()
@@ -19,8 +16,6 @@ export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={Homepage} />
-      <Route exact path="/admin" component={Admin} />
-      {/* <Route exact path="/login" component={Login} /> */}
     </Switch>
   )
 }
--- src/index.js
+++ src/index.js
@@ -7,17 +7,12 @@ import store from "./store"
 import { Provider } from "react-redux"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </Router>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
