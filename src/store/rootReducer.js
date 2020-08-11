2d10ac9161a33fc69d65273c106652400fdce857
timlaurijs <timlaurijs@gmail.com>
2020-08-11 11:52:20 +0200

    removed menuBar store

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux"
-import menuBar from "./menubar/reducer"
 import user from "./user/reducer"
 import homepageData from "./homepage/reducer"
 
 export default combineReducers({
-  menuBar,
   user,
   homepageData,
 })
