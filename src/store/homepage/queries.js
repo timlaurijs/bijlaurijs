7494c4f3222ca054afb9bb562b863cb764c13207
timlaurijs <timlaurijs@gmail.com>
2020-08-11 11:53:12 +0200

    made queries external for clarity

---
+++ src/store/homepage/queries.js
@@ -0,0 +1,26 @@
+export const fetchMenuItemsQuery = `
+* [_type=="post"] | order (order, ASC){
+  _id,
+  order,
+  title,
+  ...body[0]{
+    ...children[0]{
+     "body": text 
+    }
+  }
+} 
+`
+
+export const fetchTimelineItemsQuery = `
+* [_type=="afbeeldingen"] | order (postOrder, ASC){
+  _id,
+  ...mainImage.asset->{
+   "imageUrl": url,
+  },
+  ...post->{
+  "postId": _id,
+  "postOrder": order
+  },
+  seasons,
+}
+`
