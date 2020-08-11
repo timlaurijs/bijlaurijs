c186080604248571be175d6772ffc0fa648852bf
timlaurijs <timlaurijs@gmail.com>
2020-08-11 11:50:38 +0200

    relocated sanityClient to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import sanity from "@sanity/client"
 import ReactDOM from "react-dom"
 import "./index.scss"
 import App from "./App"
@@ -6,6 +7,15 @@ import * as serviceWorker from "./serviceWorker"
 import store from "./store"
 import { Provider } from "react-redux"
 import { BrowserRouter as Router, Route } from "react-router-dom"
+
+const { NODE_ENV, REACT_APP_SANITY_PROJECT_ID: PROJECT_ID } = process.env
+
+export const sanityClient = sanity({
+  projectId: PROJECT_ID,
+  dataset: "production",
+  useCdn: NODE_ENV === "production",
+})
+
 ReactDOM.render(
   <Provider store={store}>
     <Router>
