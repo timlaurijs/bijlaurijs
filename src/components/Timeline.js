c8429691b37d0be5e6886109282fd7f68abdcf13
timlaurijs <timlaurijs@gmail.com>
2020-08-14 23:38:46 +0200

    updated and cleaned Timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,26 +1,16 @@
-import React, { useEffect, createRef, useState } from "react"
+import React from "react"
 import "./Timeline.scss"
 import { useSelector } from "react-redux"
-import Post from "./Post"
 import { selectMenuItems } from "../store/homepage/selectors"
+import Post from "./Post"
 
 export default function Timeline() {
   const menuItems = useSelector(selectMenuItems)
-  const [itemHeight, setItemHeight] = useState(0)
-  const [itemRef, setItemRef] = useState()
-
-  // console.log("what is itemRef in Timeline?", itemRef)
-  console.log("what is itemHeight in Timeline?", itemHeight)
-
-  useEffect(() => {
-    if (!itemRef) return
-    setItemHeight(itemRef.offsetHeight)
-  }, [itemRef])
 
   return (
-    <div className="Timeline" ref={setItemRef}>
-      {menuItems.map((menu) => (
-        <Post menu={menu} key={menu._id} />
+    <div className="Timeline">
+      {menuItems.map((menuItem) => (
+        <Post key={menuItem.id} menuItem={menuItem} />
       ))}
     </div>
   )
