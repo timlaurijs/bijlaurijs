ba57cfafa2873ec174ebb9284580ca9bae3c9491
timlaurijs <timlaurijs@gmail.com>
2020-08-14 23:36:01 +0200

    added new features/updated ol features of store

--- src/store/homepage/reducer.js
+++ src/store/homepage/reducer.js
@@ -1,24 +1,51 @@
 import {
-  FETCH_POSTS_SUCCESS,
-  FETCH_MENU_ITEMS_SUCCESS,
-  setSelectedPost,
+  UPDATE_MENU_ITEMS,
+  UPDATE_POSTS,
+  UPDATE_CURRENT_MENU_ITEM,
+  UPDATE_MENU_ITEM,
 } from "./actions.js"
 
 const initialState = {
-  menuItems: [],
+  menuItems: {},
   posts: [],
-  selectedPost: [],
+  currentMenuItem: undefined,
 }
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-    case FETCH_POSTS_SUCCESS:
-      return { ...state, posts: [...state.posts, ...payload] }
-    case FETCH_MENU_ITEMS_SUCCESS:
-      return { ...state, menuItems: [...state.menuItems, ...payload] }
-    case "SET_SELECTED_POST":
-      return { ...state, selectedPost: [...payload] }
+    case UPDATE_MENU_ITEMS:
+      return {
+        ...state,
+        menuItems: {
+          ...state.menuItems,
+          // reduces payload to an object using .id as key
+          ...payload.reduce((acc, curr) => ({ ...acc, [curr.id]: curr }), {}),
+        },
+      }
 
+    //adds posts to store
+    case UPDATE_POSTS:
+      return { ...state, posts: state.posts.concat(payload) }
+
+    case UPDATE_CURRENT_MENU_ITEM:
+      return {
+        ...state,
+        // sets menuItem.id, replacing previous selected menuItem.id
+        currentMenuItem: payload,
+      }
+
+    //updates menuitem with callback function and menuItem.id
+    case UPDATE_MENU_ITEM:
+      return {
+        ...state,
+        menuItems: {
+          ...state.menuItems,
+          [payload.id]: {
+            ...(state.menuItems[payload.id] || {}),
+            ...payload.data,
+          },
+        },
+      }
     default:
       return state
   }
--- src/store/homepage/selectors.js
+++ src/store/homepage/selectors.js
@@ -1,3 +1,20 @@
-export const selectMenuItems = (state) => state.homepageData.menuItems
-export const selectedPost = (state) => state.homepageData.selectedPost
-export const selectPosts = (state) => state.homepageData.posts
+// creates array from object with menuItems
+export const selectMenuItems = (state) =>
+  Object.values(state.homepage.menuItems)
+
+// selects posts with postOrder number corresponding with menuItem.order number
+export const selectMenuItemsPosts = (menuItem) => (state) =>
+  state.homepage.posts.filter((post) => post.postOrder === menuItem.order)
+
+// create array from object of menuItemData, selects id, title, slug, callbackFn
+export const selectMenuItemData = (state) =>
+  Object.values(state.homepage.menuItems).map(
+    ({ id, title, slug, callback }) => ({
+      id,
+      title,
+      slug,
+      callback,
+    })
+  )
+
+export const selectCurrentMenuItem = (state) => state.homepage.currentMenuItem
