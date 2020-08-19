550bb2b6c0efb88f6ce2ca108b45701528ac0597
timlaurijs <timlaurijs@gmail.com>
2020-08-19 16:20:29 +0200

    added loading text to timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,15 +1,17 @@
 import React from "react"
 import "./Timeline.scss"
 import { useSelector } from "react-redux"
-import { selectMenuItems } from "../store/homepage/selectors"
+import { selectMenuItems, isLoading } from "../store/homepage/selectors"
 import Post from "./Post"
 import Map from "./Map"
 
 export default function Timeline() {
   const menuItems = useSelector(selectMenuItems)
+  const load = useSelector(isLoading)
 
   return (
     <div className="Timeline">
+      {load ? <div className="loading">Loading </div> : null}
       {menuItems.map((menuItem) => (
         <Post key={menuItem.id} menuItem={menuItem} />
       ))}
