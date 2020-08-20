543136a1379b14287275d77b73474ec9f52774d5
timlaurijs <timlaurijs@gmail.com>
2020-08-20 13:59:02 +0200

    added seasonPicker component to homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -4,12 +4,14 @@ import Menubar from "../components/Menubar"
 import BodyText from "../components/BodyText"
 import Timeline from "../components/Timeline"
 import Footer from "../components/Footer"
+import SeasonPicker from "../components/SeasonPicker"
 
 export default function Homepage() {
   return (
     <div className="Homepage">
       <div className="menu-container">
         <Menubar />
+        <SeasonPicker />
         <BodyText />
       </div>
       <Timeline />
